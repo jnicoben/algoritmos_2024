6c1f74aa8deb92a43fe966c2d9ca336c67f80fee
Nicolas Benitez <jnicoben95@gmail.com>
2024-05-22 11:51:32 -0300

    new commit

---
+++ pila.py
@@ -0,0 +1,11 @@
+class Stack:
+    def __init__(self):
+        self.elements = []
+    def push(self):
+        pass
+    def pop(self):
+        pass
+    def on_top(self):
+        pass
+    def size(self):
+        pass
