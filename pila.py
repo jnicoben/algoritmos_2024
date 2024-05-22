05e281fd44c6584026db70468fada8758354be37
Nicolas Benitez <jnicoben95@gmail.com>
2024-05-22 18:35:48 -0300

    modificacion comentarios

--- pila.py
+++ pila.py
@@ -1,17 +1,24 @@
 class Stack:
     def __init__(self):
-        self.elements = [] #El self es el inicializador de la variable, similar al .this de java.
-    def push(self, element): #El push seria el apilar, con append. 
-        #Agregamos un metodo .append() por que tiene que entrar en el ultimo lugar de la lista. 
-        #Y en el insert tendriamos q indicar la posicion.
+        self.elements = [] #*El self es el inicializador de la variable, similar al .this de java.
+    def push(self, element): 
         self.elements.append(element)
+        #! El push seria el apilar, con append. 
+        #! Agregamos un metodo .append() por que tiene que entrar en el ultimo lugar de la lista. 
+        # Y el metodo .insert() tendriamos q indicar la 
+        # posicion para insertar el elemento en una posicion particular de la lista.
+    
     def pop(self):
-        pass
+        self.elements.pop() #*No se indica el parametro al metodo .pop(), ya que elimina el ultimo elemento de la lista por defecto (pila).
     def on_top(self):
         pass
+    
     def size(self):
         pass
 
-#Ejemplo del .append
+#!Ejemplo del .append
+"""
 fruits = ['apple', 'banana', 'cherry']
-fruits.append("orange")
+!fruits.append("orange")
+print(fruits)
+"""
